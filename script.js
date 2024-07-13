6cd84b6f55118a7fe0f54fcd662ac9bf044fbe1d
Junior Lawrence Kibirige <aanga26@gmail.com>
2024-07-13 21:18:44 +0300

    changed the days to use

--- script.js
+++ script.js
@@ -6,27 +6,26 @@ const minutes = document.querySelector('.minutes')
 const seconds = document.querySelector('.seconds')
 
 let check = []
-var distance = 830064;
+var countDownDate = new Date("Jul 17, 2024 15:37:25").getTime();
+// Get today's date and time
+var now = new Date().getTime();
+var distance = countDownDate - now;
 
 function countDown() {
     const times = [{}, {}, {}, {}]
-    let total = parseInt(localStorage.getItem('time'))
-    localStorage.setItem('time', total - 1)
-    let s1 = Math.floor(total % 60)
+    let s1 = Math.floor((distance % (1000 * 60)) / 1000);
     times[3].front = s1
     times[3].back = (s1 === 0 ? 59 : s1 - 1)
 
-    total /= 60
-    let m1 = Math.floor(total % 60)
+    let m1 = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     times[2].front = m1
     times[2].back = (m1 === 0 ? 59 : m1 - 1)
 
-    total /= 60
-    let h1 = Math.floor(total % 24)
+    let h1 = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     times[1].front = h1
     times[1].back = (h1 === 0 ? 23 : h1 - 1)
 
-    let d1 = Math.floor(total / 24)
+    let d1 = Math.floor(distance / (1000 * 60 * 60 * 24));
     times[0].front = d1
     times[0].back = (d1 === 0 ? 0 : d1 - 1)
 
@@ -65,14 +64,6 @@ function countDown() {
     if (seconds.dataset.before == '00' && minutes.dataset.before == '00' && hours.dataset.before == '00') {
         days.parentElement.classList.add('flip')
     }
-    if (seconds.dataset.before == '01' && minutes.dataset.before == '00' && hours.dataset.before == '00' && days.dataset.before == '00') {
-        localStorage.setItem('time', distance)
-
-    }
-}
-
-if (!localStorage.getItem('time')) {
-    localStorage.setItem('time', distance);
 }
 
 countDown()
